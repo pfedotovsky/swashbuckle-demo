709f0e69d943d8d7447c73a8227a8badb8f3c69b
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2017-04-28 19:49:18 +0300

    Added simple API

---
+++ Swashbuckle/Swashbuckle/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.Models;
+
+namespace Swashbuckle.Controllers
+{
+    [Route("products/")]
+    public class ProductsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Retrieves a specific product by unique id
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Product created</response>
+        /// <response code="500">Oops! Can't create your product right now</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Product), 200)]
+        [ProducesResponseType(typeof(void), 500)]
+        public Product Get(int id)
+        {
+            return new Product
+            {
+                Description = "Description",
+                Name = "Name",
+                ProductID = 1,
+                ProductTypeID = 1,
+                Valid = true
+            };
+        }
+
+        // POST products
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), 200)]
+        [ProducesResponseType(typeof(void), 500)]
+        public void Post([FromBody]Product value)
+        {
+        }
+    }
+}
---
+++ Swashbuckle/Swashbuckle/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Swashbuckle.Models
+{
+    public class Product
+    {
+        public int? ProductID { get; set; }
+
+        /// <summary>
+        ///  Product type id.
+        /// </summary>
+        public int? ProductTypeID { get; set; }
+
+        /// <summary>
+        ///  Product name.
+        /// </summary>
+        [DefaultValue("Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        ///  Product description.
+        /// </summary>
+        [DefaultValue("Description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        ///  Is valid product.
+        /// </summary>
+        public bool? Valid { get; set; }
+    }
+}
